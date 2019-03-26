a5375e23db28e1ea0b0cdf5e0a8fb0c40cb80fb8
Jens Brink <jensbrink@Jenss-MacBook-Pro.local>
2019-03-26 11:31:24 +0100

    Jens/AlphaVantage

---
+++ Project 1.py
@@ -0,0 +1,4 @@
+# DOWNLOAD STOCK DATA LINK https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&outputsize=full&apikey=XU0S88ZM101LB0FQ&datatype=csv
+# CHANGE "SYMBOL" TO TICKER-CODE. AAPL FOR NOW.
+
+
