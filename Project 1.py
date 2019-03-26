97c7e90ca4249c86446e12c60237eae443b02e4d
Jens Brink <jensbrink@Jenss-MacBook-Pro.local>
2019-03-26 15:47:53 +0100

    Update

--- Project 1.py
+++ Project 1.py
@@ -1,23 +1,29 @@
-# DOWNLOAD STOCK DATA LINK https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&outputsize=full&apikey=XU0S88ZM101LB0FQ&datatype=csv
-###----- TEST PYTHON -----###
-print("Så kører vi!") # Virker Python overhovedet?
-###----- START KODE -----###
+# EXAMPLE LINK: https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&outputsize=full&apikey=XU0S88ZM101LB0FQ&datatype=csv
+
+# Import neccesary modules and packages #
 import pandas as pd
+import matplotlib.pyplot as plt
+import time
 
-apikey = "XU0S88ZM101LB0FQ"
-outputsize = "compact" # Compact or full. Compact is 100 points, full is 20+ years.
+# SET ATTRIBUTES FOR DATA FETCH #
+apikey = "XU0S88ZM101LB0FQ" 
+outputsize = "compact" # Compact or full. Compact is 100 days, full is 20+ years.
+tickers = ["AAPL", "TSLA", "TEVA"] # MAX of five 
 
-stocks_df = pd.DataFrame()
-tickers = ["AAPL", "TSLA", "TEVA"]
-print("\n-----------------------------------\n")
-print("Based on individual dataframes:")
+# Empty Panda DataFrame to append individual stock data to 
+stocks_df = pd.DataFrame() 
+
+# Loop through tickercodes and fetch data individually, then append to stocks_df
 for ticker in tickers:
-    fetch_link = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + ticker + "&outputsize=" + outputsize + "&apikey=" + apikey + "&datatype=csv"
-    df = pd.read_csv(fetch_link)
-    df["ticker"] = ticker
-    print("Mean close for " + ticker +  " is " + str(round(df.close.mean(),2)))
-    stocks_df = stocks_df.append(df,ignore_index=True)
-print("\nBased on aggregate dataframe:")
-print("Mean close for all stocks is " + str(round(stocks_df.close.mean(),2)))
-print("\n-----------------------------------\n")
-print(stocks_df)
+    fetch_link = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + ticker + "&outputsize=" + outputsize + "&apikey=" + apikey + "&datatype=csv" # Building each fetch link
+    df = pd.read_csv(fetch_link) # Load CSV from above URL to df
+    df["ticker"] = ticker # Add ticker-column for identification after appending
+    print("Mean close for " + ticker +  " is " + str(round(df.close.mean(),2))) #Print mean of each, just a check
+    stocks_df = stocks_df.append(df,ignore_index=True) # Append df for each stock to stocks_df
+# Loop end
+
+#Data cleaning and analysis begin
+print("Mean close for all stocks is " + str(round(stocks_df.close.mean(),2))) # Print mean of all shares for check
+
+
+
