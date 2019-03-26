305edc1abb5aeeefe705b72d50e598a1a3ffaa21
Jens Brink <jensbrink@Jenss-MacBook-Pro.local>
2019-03-26 12:54:57 +0100

    Proof of concept/AlphaVantage

--- Project 1.py
+++ Project 1.py
@@ -8,11 +8,13 @@ outputsize = "compact" # Compact or full. Compact is 100 points, full is 20+ yea
 apikey = "XU0S88ZM101LB0FQ" # Max 5 fetches per minute per apikey. Alternative keys:
 
 stocks_df = pd.DataFrame()
-tickers = ["AAPL", "TSLA"]
+tickers = ["AAPL", "TSLA","TEVA"]
 for ticker in tickers:
     fetch_link = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + ticker + "&outputsize=" + outputsize + "&apikey=" + apikey + "&datatype=csv"
     df = pd.read_csv(fetch_link)
+    df["ticker"] = ticker
     print("Mean close for " + ticker +  " is " + str(round(df.close.mean(),2)))
     stocks_df = stocks_df.append(df,ignore_index=True)
 
-print("\nMean close for all stocks is " + str(round(stocks_df.close.mean(),2)))
+print("Mean close for all stocks is " + str(round(stocks_df.close.mean(),2)))
+print(stocks_df)
