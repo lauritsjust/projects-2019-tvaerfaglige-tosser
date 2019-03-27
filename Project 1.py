2cca2cc2f7397754eb4dc6652f07cfc382b1a337
Jens Brink <jensbrink@Jenss-MacBook-Pro.local>
2019-03-27 14:13:54 +0100

    Jens

--- Project 1.py
+++ Project 1.py
@@ -9,7 +9,7 @@ import matplotlib.dates as mdates
 # SET ATTRIBUTES FOR DATA FETCH #
 apikey = "XU0S88ZM101LB0FQ" 
 outputsize = "compact" # Compact or full. Compact is 100 days, full is 20+ years.
-tickers = ["AAPL", "TSLA", "TEVA"] # Maximum of five stocks per data fetch and only one data fetch per minute allowed
+tickers = ["AAPL", "TSLA", "AIG", "FB"] # Maximum of five stocks per data fetch and only one data fetch per minute allowed
 
 # Empty Panda DataFrame to append individual stock data to 
 stocks_df = pd.DataFrame() 
@@ -26,14 +26,16 @@ print("Mean close for all stocks is " + str(round(stocks_df.close.mean(),2)))
 print("\n-----------------------------------\n")
 print(stocks_df)
 #Vi laver nu en graf der viser udviklingen
-#fig = plt.figure(figsize=(10,5))
-#ax = fig.add_subplot(1,1,1)
-#ax.set_title('ticker_name')
-#timestamp=stocks_df.loc[stocks_df['ticker'] =='AAPL', ['timestamp']]
-#timestamp2=timestamp['timestamp']
-#close=stocks_df.loc[stocks_df['ticker'] =='AAPL', ['close']]
-#x.plot(timestamp2,close)
-ax = stocks_df.plot(x_compat=True)
-ax.xaxis.set_major_locator(mdates.MonthLocator())
-
-plt.show()
+for ticker in tickers:
+    get_stock=stocks_df.loc[stocks_df['ticker'] == ticker, :]
+    get_stock_timestamp = get_stock['timestamp']
+    get_stock_close = get_stock['close']
+    plt.plot(get_stock_timestamp,get_stock_close)
+    plt.title(ticker)
+    #HER HAR JEG BARE LAVET TICKS PÅ 0%, 25%, 50%, 75% og 100%.
+    days = get_stock_timestamp.size # Antallet af dage i timestamp
+    ticks = [0, 0.25*days, 0.5*days, 0.75*days, days]
+    # VI SKAL FINDE LOKATIONEN FOR FØRSTE I HVER MÅNED OG SÆTTE IND I STEDET
+    # HERUNDER LAVES X-AKSEN
+    plt.xticks(ticks)
+    plt.show()
