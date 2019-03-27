e15874ebfef532e441b2b702f8f6ff934a9356eb
Jens Brink <jensbrink@Jenss-MacBook-Pro.local>
2019-03-27 15:43:29 +0100

    Update (x-ticks and freq option)

--- Project 1.py
+++ Project 1.py
@@ -3,31 +3,30 @@
 # Import neccesary modules and packages #
 import pandas as pd
 import matplotlib.pyplot as plt
-import time
-import matplotlib.dates as mdates
 
 # SET ATTRIBUTES FOR DATA FETCH #
 apikey = "XU0S88ZM101LB0FQ" 
-outputsize = "compact" # Compact or full. Compact is 100 days, full is 20+ years.
+outputsize = "compact" # Compact or full, if daily data. Compact is 100 days, full is 20+ years.
 tickers = ["AAPL", "TSLA", "AIG", "FB"] # Maximum of five stocks per data fetch and only one data fetch per minute allowed
+freq = "DAILY" # DAILY, WEEKLY OR MONTHLY
 
 # Empty Panda DataFrame to append individual stock data to 
 stocks_df = pd.DataFrame() 
 
 # Loop through tickercodes and fetch data individually, then append to stocks_df
 for ticker in tickers:
-    fetch_link = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + ticker + "&outputsize=" + outputsize + "&apikey=" + apikey + "&datatype=csv"
+    fetch_link = "https://www.alphavantage.co/query?function=TIME_SERIES_" + freq + "&symbol=" + ticker + "&outputsize=" + outputsize + "&apikey=" + apikey + "&datatype=csv"
     df = pd.read_csv(fetch_link)
     df["ticker"] = ticker
     print("Mean close for " + ticker +  " is " + str(round(df.close.mean(),2)))
     stocks_df = stocks_df.append(df,ignore_index=True)
-print("\nBased on aggregate dataframe:")
 print("Mean close for all stocks is " + str(round(stocks_df.close.mean(),2)))
 print("\n-----------------------------------\n")
 print(stocks_df)
 #Vi laver nu en graf der viser udviklingen
 for ticker in tickers:
     get_stock=stocks_df.loc[stocks_df['ticker'] == ticker, :]
+    get_stock = get_stock[::-1] # Reverses order
     get_stock_timestamp = get_stock['timestamp']
     get_stock_close = get_stock['close']
     plt.plot(get_stock_timestamp,get_stock_close)
