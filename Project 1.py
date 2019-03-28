9f423a12db0b625c73dc9aa04a472cd4ee0eaf12
Jens Brink <jensbrink@Jenss-MacBook-Pro.local>
2019-03-28 15:21:32 +0100

    Jens måneds-ticker

--- Project 1.py
+++ Project 1.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 # SET ATTRIBUTES FOR DATA FETCH #
 apikey = "XU0S88ZM101LB0FQ" 
 outputsize = "compact" # Compact or full, if daily data. Compact is 100 days, full is 20+ years.
-tickers = ["AAPL", "TSLA", "AIG", "FB"] # Maximum of five stocks per data fetch and only one data fetch per minute allowed
+tickers = ["BA","AAPL"] # Maximum of five stocks per data fetch and only one data fetch per minute allowed
 freq = "DAILY" # DAILY, WEEKLY OR MONTHLY
 
 # Empty Panda DataFrame to append individual stock data to 
@@ -23,18 +23,21 @@ for ticker in tickers:
 print("Mean close for all stocks is " + str(round(stocks_df.close.mean(),2)))
 print("\n-----------------------------------\n")
 print(stocks_df)
+
+
 #Vi laver nu en graf der viser udviklingen
 for ticker in tickers:
     get_stock=stocks_df.loc[stocks_df['ticker'] == ticker, :]
-    get_stock = get_stock[::-1] # Reverses order
+    get_stock = get_stock[::-1] # Reverses order of dataframe
+    get_stock = get_stock.reset_index() # Re-indexes
+    print(get_stock)
     get_stock_timestamp = get_stock['timestamp']
     get_stock_close = get_stock['close']
     plt.plot(get_stock_timestamp,get_stock_close)
     plt.title(ticker)
-    #HER HAR JEG BARE LAVET TICKS PÅ 0%, 25%, 50%, 75% og 100%.
-    days = get_stock_timestamp.size # Antallet af dage i timestamp
-    ticks = [0, 0.25*days, 0.5*days, 0.75*days, days]
-    # VI SKAL FINDE LOKATIONEN FOR FØRSTE I HVER MÅNED OG SÆTTE IND I STEDET
     # HERUNDER LAVES X-AKSEN
-    plt.xticks(ticks)
-    plt.show()
+    #first_day_of_month = stocks_df.loc[get_stock['timestamp'].str[-2:] == "01", :].index.tolist()
+    first_trading_day_of_month = get_stock.loc[get_stock['timestamp'].str[-2:] <= get_stock['timestamp'].str[-2:].shift(periods=1),:].index.tolist()
+    plt.xticks(first_trading_day_of_month)
+    plt.xticks(rotation=90)
+    plt.show()    
