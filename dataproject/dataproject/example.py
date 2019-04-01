f06c15ed02c42a3907393cb0641f760e0b927511
Jens Brink <jensbrink@Jenss-MacBook-Pro.local>
2019-04-01 15:35:26 +0200

    Jens

--- dataproject/dataproject/example.py
+++ dataproject/dataproject/example.py
@@ -13,10 +13,10 @@ def fetch_data(tickers):
 
     # Loop through tickercodes and fetch data individually, then append to stocks_df
     for ticker in tickers:
-    fetch_link = "https://www.alphavantage.co/query?function=TIME_SERIES_" + freq + "_ADJUSTED&symbol=" + ticker + "&outputsize=" + outputsize + "&apikey=" + apikey + "&datatype=csv"
-    df = pd.read_csv(fetch_link)
-    df["ticker"] = ticker
-    print("Mean close for " + ticker +  " is " + str(round(df.close.mean(),2)))
-    stocks_df = stocks_df.append(df,ignore_index=True)
-    print("Mean close for all stocks is " + str(round(stocks_df.close.mean(),2)))
-    print("\n-----------------------------------\n")
+        fetch_link = "https://www.alphavantage.co/query?function=TIME_SERIES_" + freq + "_ADJUSTED&symbol=" + ticker + "&outputsize=" + outputsize + "&apikey=" + apikey + "&datatype=csv"
+        df = pd.read_csv(fetch_link)
+        df["ticker"] = ticker
+        print("Mean close for " + ticker +  " is " + str(round(df.close.mean(),2)))
+        stocks_df = stocks_df.append(df,ignore_index=True)
+        print("Mean close for all stocks is " + str(round(stocks_df.close.mean(),2)))
+        print("\n-----------------------------------\n")
